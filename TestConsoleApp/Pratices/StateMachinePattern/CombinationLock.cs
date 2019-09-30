f07d76e15cd11b292e9b026e61d402a4fad11cd7
Salatiel Bairros <salatiel.costabairros@gmail.com>
2019-09-30 15:01:29 -0300

    State Design Pattern

---
+++ TestConsoleApp/Pratices/StateMachinePattern/CombinationLock.cs
@@ -0,0 +1,39 @@
+﻿namespace CodeTestApp.Pratices.StateMachinePattern
+{
+  public class CombinationLock
+  {
+    public string Status;
+    private readonly int[] _combination;
+    private int informedCountIndex = 0;
+
+    public CombinationLock(int[] combination)
+    {
+      _combination = combination;
+      Lock();
+    }
+
+    public void Lock()
+    {
+      Status = "LOCKED";
+      informedCountIndex = 0;
+    }
+
+    public void EnterDigit(int digit)
+    {
+      if (informedCountIndex == 0) Status = string.Empty;
+      if (_combination[informedCountIndex] != digit)
+      {
+        Status = "ERROR";
+      }
+      else if ((informedCountIndex + 1) == _combination.Length)
+      {
+        Status = "OPEN";
+      }
+      else
+      {
+        Status += digit;
+        informedCountIndex++;
+      }
+    }
+  }
+}
